870b982b401d96255caec7be2f39bca1ece7e58b
ArchieRinn <brilliandar9428@gmail.com>
2021-01-03 09:19:44 +0900

    fix bug change

--- src/components/DialogForm.js
+++ src/components/DialogForm.js
@@ -28,9 +28,11 @@ const DialogForm = () => {
   const handleClose = () => {
     setOpen(false);
     if (editedItem.quantity > 0) {
+      if (!idArray.includes(editedItem.id)) {
     const quantity = total.quantity > 0 ? Number(total.quantity) - Number(editedItem.quantity) : 0;
     const price = total.price > 0 ? Number(total.price) - (editedItem.price * editedItem.quantity) : 0;
     setTotal({...total, price, quantity});
+      }
     }
     setTimeout(() => {
       setEditedItem({
@@ -77,11 +79,45 @@ const DialogForm = () => {
     }, 100)
   };
 
+  const formatNumber = (number) => {
+    const num = number.toString();
+    const length = num.length - 1;
+    if (length >= 3) {
+      const mod = length % 3;
+      if (mod > 0) {
+        return num.substring(0, mod+1) + "." + num.substring(mod+1);
+      } else if (length / 3 === 1) {
+        return num.substring(0, 1) + "." + num.substring(1);
+      } else {
+        let i = 0;
+        let output = "";
+        while (i < length / 3) {
+          const index = i * 3;
+          if (index === 0) {
+            output += num.substring(0, 1) + "." + num.substring(1, 3);
+          } else if (i + 1 === length / 3) {
+            output +=
+              num.substring(index, index + 1) + "." + num.substring(index + 1);
+          } else {
+            output +=
+              num.substring(index, index + 1) +
+              "." +
+              num.substring(index + 1, index + 3);
+          }
+          i++;
+        }
+        return output;
+      }
+    } else {
+      return num;
+    }
+  };
+
   return (
     <Dialog fullWidth={true} maxWidth="lg" open={open} onClose={handleClose}>
       <DialogTitle>
         <Typography variant="h5">{editedItem.name}</Typography>
-        <Typography variant="h6">{editedItem.price}</Typography>
+        <Typography variant="h6">{`Rp${formatNumber(Number(editedItem.price))}`}</Typography>
       </DialogTitle>
       <DialogContent>
         <Form />
